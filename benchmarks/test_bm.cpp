8bbac29fb427a91ca1b2749b26d4e71e22e9f8c4
KexiMleko <ocokolja.aleksa@gmail.com>
2026-06-24 15:41:40 +0200

    build: add initial build system

---
+++ benchmarks/test_bm.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include <stdio.h>
+#include "task_runtime.hpp"
+
+int main(){
+
+    printf("Benchmark invoked\n");
+    init_tr();
+    return EXIT_SUCCESS;
+}
---
+++ src/init.cpp
@@ -0,0 +1,6 @@
+#include "task_runtime.hpp"
+#include <cstdio>
+void init_tr() { 
+    printf("Task runtime init invoked\n");
+    return; 
+}
